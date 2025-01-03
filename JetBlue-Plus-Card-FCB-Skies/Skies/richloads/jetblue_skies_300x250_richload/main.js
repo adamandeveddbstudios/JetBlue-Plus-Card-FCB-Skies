7702895b50dfab4ca698812ac3500e63d01e048a
Mahesh Bhatt <141926871+adamandeveddbstudios@users.noreply.github.com>
2025-01-03 20:35:57 +0530

    Add files via upload

--- JetBlue-Plus-Card-FCB-Skies/Skies/richloads/jetblue_skies_300x250_richload/main.js
+++ JetBlue-Plus-Card-FCB-Skies/Skies/richloads/jetblue_skies_300x250_richload/main.js
@@ -33,36 +33,17 @@ function animate() {
     .to('#container', 0.5, { autoAlpha: 1 }, 'start+=0.5')
     .staggerTo(['#copy01', '#copy02'], 1, { autoAlpha: 1, ease: "expo.out" }, 0.1, 'frame1+=0.5')
     .staggerFrom(['#copy01', '#copy02'], 1, { y: '50', ease: "expo.out" }, 0.1, 'frame1+=0.5')
-    
     .to('#copy02', 1, {y:'65'}, 'frame1+=3')
-
     .staggerTo(['#copy03'], 1, { autoAlpha: 1, ease: "expo.out" }, 0.1, 'frame1+=3.5')
     .staggerFrom(['#copy03'], 1, { y: '100', ease: "expo.out" }, 0.1, 'frame1+=3.5')
-
     .to(['#copy01', '#copy02', '#copy03'], 0.3, {autoAlpha:0}, 'frame1+=6')
-
+    
     tl.addLabel('frame2', 6)
     .staggerTo(['#copy04', '#copy05', '#copy06'], 1, { autoAlpha: 1, ease: "expo.out" }, 0.1, 'frame2+=0.5')
     .staggerFrom(['#copy04', '#copy05', '#copy06'], 1, { y: '50', ease: "expo.out" }, 0.1, 'frame2+=0.5')
-
     .to('#legal1', 0.3, {autoAlpha:0}, 'frame2')
-
-    
-
-    // .to('.copyF1', 0.5, {alpha: 0, delay: 2})
-    // .staggerTo(['#copy4', '#copy5', '#copy6'], 1, { autoAlpha: 1, ease: "expo.out" }, 0.1, 'frame1+=6.5')
-    // .staggerFrom(['#copy4', '#copy5', '#copy6'], 1, { y: '+=200', ease: "expo.out" }, 0.1, 'frame1+=6.5')
-    // .to('#footer', 0.5, {css: { 'opacity': '0'}, delay: 1.5})
-    // .to('#footer', 0.1, {css: {'filter': 'brightness(0) invert(1)', delay: 1.5}})
-
     .to('#endframe', 1, { top: 0, ease: "expo.out" }, 'frame2+=4')
-    // .to('#firstCard', 0.3, {autoAlpha:0}, 'frame2')
-    // .to('#footer', 0.5, {css: { 'opacity': '1'}})
     .to('#shine', 0.5, {backgroundPosition: '450px 0px'});
-
-    // tl.to(['#sand'], 10, { transform: "translate3d(42px, 8px, 1px) scale(1.05)", ease: Power1.easeInOut }, 0)
-    // tl.to(['#sea'], 10, { transform: "translate3d(25px, 10px, 1px) scale(1.15)", ease: Power1.easeInOut }, 0)
-    // tl.to(['#sky'], 10, { transform: "translate3d(0px, 0px, 1px) scale(1.3)", ease: Power1.easeInOut }, 0)
 
 }
 
